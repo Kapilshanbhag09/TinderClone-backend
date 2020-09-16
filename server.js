aaf92ca8ad7970c6436afe1588de5d3dd765476f
Kapilshanbhag09 <shanbhag.kapil09@gmail.com>
2020-09-16 22:38:35 +0530

    tinderbackend

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Cards from "./dbCards.js";
+import Cors from "cors";
+//App config
+const app= express();
+const port= process.env.PORT || 8001
+const conn_url='mongodb+srv://admintinderclone:TeXdRhHW6xBY6wg@cluster0.fbwbz.mongodb.net/tinderdb?retryWrites=true&w=majority'
+
+// Middlewares
+app.use(express.json())
+app.use(Cors())
+//DB config
+mongoose.connect(conn_url,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+//API Endpoints
+app.get('/',(req,res)=>res.status(200).send('Hello Kapil'));
+
+app.post('/tinder/cards',(req,res)=>{
+    const dbCard = req.body;
+    Cards.create(dbCard, (err,data)=>{
+        if (err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(200).send(data)
+        }
+    })
+});
+    app.get('/tinder/cards',(req,res)=>{
+        Cards.find((err,data)=>{
+            if (err){
+                res.status(500).send(err)
+            }
+            else{
+                res.status(200).send(data)
+            }
+
+    });
+});
+//Listener
+app.listen(port, ()=>console.log('listning on localhost: '+port))
